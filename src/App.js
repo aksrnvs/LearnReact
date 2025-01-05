52ddc372d7085849f85d2849dad158c65b838ad1
Kalyana Srinivas <kalyan@Kalyans-MacBook-Air.local>
2025-01-06 02:23:02 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// Filename - App.js
+import React from 'react';
+
+function App() {
+    const headingStyle = {
+        color: 'green',
+        textAlign: 'center'
+    };
+
+    return <h1 style={headingStyle}>Welcome To GeeksforGeeks!</h1>;
+}
+
+export default App;
